ab9cdb5f3490a61f3a04fa8c63a54183d635f5f0
josedgr <josedgr83@gmail.com>
2015-05-23 16:28:10 +0100

    Corregido error del Check
    "Solucionado" cuando no esta ningun check marcado

--- js/index.js
+++ js/index.js
@@ -13,30 +13,25 @@ function itemNuevo(){ //comprobando los checked y escribiendo los paises
 	var clave1=document.getElementById("pais1");
 	var clave2=document.getElementById("pais2");
 	var clave3=document.getElementById("pais3");
-	//var zonadatos1=document.getElementById("zona_paises1");
-	//var zonadatos2=document.getElementById("zona_paises2");
-	//var zonadatos3=document.getElementById("zona_paises3");
 	
 	if (clave1.checked)
 		zona_paises1.innerHTML="<div>Hola 1</div>";
+		else
+		zona_paises1.innerHTML="";
 		if (clave2.checked)
 			zona_paises2.innerHTML="<div>Hola 2</div>";
+			else
+				zona_paises2.innerHTML="";
 				if (clave3.checked)
 					zona_paises3.innerHTML="<div>Hola 3</div>";
+					else
+					zona_paises3.innerHTML="";
+					
+	if (!clave1.checked && !clave2.checked && !clave3.checked) // si no esta ninguna seleccionada
+		alert("error"); //añadir lo que falta para no avanzar de pagina
 	
-		alert(total);
 }
 
-function mostrar(clave){ //grabando en la web los paises checked
-	
-	
-	var zonadatos=document.getElementById("zonadatos");
-	
-	
-	
-	zonadatos.innerHTML="<div>Hola "+ clave + "</div>";
-	
-}
 
 
 
