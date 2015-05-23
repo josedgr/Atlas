7c2b7de0d56beef2e5c038c0b014deabd6da7ae2
josedgr <josedgr83@gmail.com>
2015-05-23 16:17:27 +0100

    modificado europa checkbox
    checkbox funcionando falta poner texto y foto (un poco a lo bruto9

--- js/index.js
+++ js/index.js
@@ -1,26 +1,38 @@
 
 function comenzar(){
-	var boton=document.getElementById("btn_saludo");
+	var boton=document.getElementById("btn_ver_paises");
 	
 	boton.addEventListener("click",itemNuevo, false);
 	
 	
 	}
 
-function itemNuevo(){
+function itemNuevo(){ //comprobando los checked y escribiendo los paises
 	
-	var clave=document.getElementById("nombre").value;
 	
-	//sessionStorage.setItem(clave);
-	mostrar(clave);
+	var clave1=document.getElementById("pais1");
+	var clave2=document.getElementById("pais2");
+	var clave3=document.getElementById("pais3");
+	//var zonadatos1=document.getElementById("zona_paises1");
+	//var zonadatos2=document.getElementById("zona_paises2");
+	//var zonadatos3=document.getElementById("zona_paises3");
+	
+	if (clave1.checked)
+		zona_paises1.innerHTML="<div>Hola 1</div>";
+		if (clave2.checked)
+			zona_paises2.innerHTML="<div>Hola 2</div>";
+				if (clave3.checked)
+					zona_paises3.innerHTML="<div>Hola 3</div>";
+	
+		alert(total);
 }
 
-function mostrar(clave){
-	//sessionStorage.getItem(clave)
+function mostrar(clave){ //grabando en la web los paises checked
+	
 	
 	var zonadatos=document.getElementById("zonadatos");
 	
-	//var elvalor=sessionStorage.getItem(clave, );
+	
 	
 	zonadatos.innerHTML="<div>Hola "+ clave + "</div>";
 	
