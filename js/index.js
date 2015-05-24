7266a2d27ccdbe23c18ac3aa66e1a304682810dd
Tanatosk1 <dvragr06@gmail.com>
2015-05-24 19:43:10 +0100

    last js

--- js/index.js
+++ js/index.js
@@ -12,15 +12,18 @@ function itemNuevo(){ //comprobando los checked y escribiendo los paises
 	var clave1=document.getElementById("pais1");
 	var clave2=document.getElementById("pais2");
 	var clave3=document.getElementById("pais3");
-	var clave3=document.getElementById("pais4");
-	var clave3=document.getElementById("pais5");
-	var clave3=document.getElementById("pais6");
-	var clave3=document.getElementById("pais7");
-	var clave3=document.getElementById("pais8");
-	var clave3=document.getElementById("pais9");
-	var clave3=document.getElementById("pais10");
-	var clave3=document.getElementById("pais11");
-	var clave3=document.getElementById("pais12");
+	var clave4=document.getElementById("pais4");
+	var clave5=document.getElementById("pais5");
+	var clave6=document.getElementById("pais6");
+	var clave7=document.getElementById("pais7");
+	var clave8=document.getElementById("pais8");
+	var clave9=document.getElementById("pais9");
+	var clave10=document.getElementById("pais10");
+	var clave11=document.getElementById("pais11");
+	var clave12=document.getElementById("pais12");
+	var clave13=document.getElementById("pais13");
+	var clave14=document.getElementById("pais14");
+	var clave15=document.getElementById("pais15");
 
 	
 	if (clave1.checked)
@@ -35,51 +38,51 @@ function itemNuevo(){ //comprobando los checked y escribiendo los paises
 		zona_paises3.innerHTML="<div>Italia</div>";
 	else
 		zona_paises3.innerHTML="";
-	if (clave1.checked)
+	if (clave4.checked)
 		zona_paises4.innerHTML="<div>Marruecos</div>";
 	else
 		zona_paises4.innerHTML="";
-	if (clave1.checked)
+	if (clave5.checked)
 		zona_paises5.innerHTML="<div>Egipto</div>";
 	else
 		zona_paises5.innerHTML="";
-	if (clave1.checked)
+	if (clave6.checked)
 		zona_paises6.innerHTML="<div>Kenia</div>";
 	else
 		zona_paises6.innerHTML="";
-	if (clave1.checked)
+	if (clave7.checked)
 		zona_paises7.innerHTML="<div>China</div>";
 	else
 		zona_paises7.innerHTML="";
-	if (clave1.checked)
+	if (clave8.checked)
 		zona_paises8.innerHTML="<div>Japon</div>";
 	else
 		zona_paises8.innerHTML="";
-	if (clave1.checked)
+	if (clave9.checked)
 		zona_paises9.innerHTML="<div>Mongolia</div>";
 	else
 		zona_paises9.innerHTML="";
-	if (clave1.checked)
+	if (clave10.checked)
 		zona_paises10.innerHTML="<div>Estados Unidos</div>";
 	else
 		zona_paises10.innerHTML="";
-	if (clave1.checked)
+	if (clave11.checked)
 		zona_paises11.innerHTML="<div>Chile</div>";
 	else
 		zona_paises11.innerHTML="";
-	if (clave1.checked)
+	if (clave12.checked)
 		zona_paises12.innerHTML="<div>Argentina</div>";
 	else
 		zona_paises12.innerHTML="";
-	if (clave1.checked)
+	if (clave13.checked)
 		zona_paises13.innerHTML="<div>Australia/div>";
 	else
 		zona_paises13.innerHTML="";
-	if (clave1.checked)
+	if (clave14.checked)
 		zona_paises14.innerHTML="<div>Fiyi</div>";
 	else
 		zona_paises14.innerHTML="";
-	if (clave1.checked)
+	if (clave15.checked)
 		zona_paises15.innerHTML="<div>Nueva Zelanda</div>";
 	else
 		zona_paises15.innerHTML="";
