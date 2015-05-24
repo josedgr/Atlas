6142af261a88d13f6fe883473595e4c486241a9b
josedgr <josedgr83@gmail.com>
2015-05-24 19:10:53 +0100

    js funcionando
    js funcionando

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ function comenzar(){
 	
 	boton.addEventListener("click",itemNuevo, false);
 	
-	
 	}
 
 function itemNuevo(){ //comprobando los checked y escribiendo los paises
@@ -16,21 +15,26 @@ function itemNuevo(){ //comprobando los checked y escribiendo los paises
 	
 	if (clave1.checked)
 		zona_paises1.innerHTML="<div>Alemania</div>";
-		else
+	else
 		zona_paises1.innerHTML="";
-		if (clave2.checked)
-			zona_paises2.innerHTML="<div>España</div>";
-			else
-				zona_paises2.innerHTML="";
-				if (clave3.checked)
-					zona_paises3.innerHTML="<div>Italia</div>";
-					else
-					zona_paises3.innerHTML="";
+	if (clave2.checked)
+		zona_paises2.innerHTML="<div>España</div>";
+	else
+		zona_paises2.innerHTML="";
+	if (clave3.checked)
+		zona_paises3.innerHTML="<div>Italia</div>";
+	else
+		zona_paises3.innerHTML="";
 					
 	if (!clave1.checked && !clave2.checked && !clave3.checked) // si no esta ninguna seleccionada
 		alert("error"); //añadir lo que falta para no avanzar de pagina
+		
+	if (clave1.checked || clave2.checked || clave3.checked)
+		window.location.href ="#PaisesE";
 	
 }
+
+
 
 
 
